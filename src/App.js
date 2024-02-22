c73d47441c445223377c17d1196cefae9acf0ca0
abhilashvs2012003 <abhilash.brototype@gmail.com>
2024-02-22 18:36:48 +0530

    To Do App created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react"
+import {TaskList,CompletedTaskList,Time,Day} from "./components/taskList"
 function App() {
+  const [toDos,setToDos] = useState([])
+  const [toDo,setToDo] = useState('')
+  const time = new Date().toDateString()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App d-flex flex-column align-item-center justify-content-center" style={{  }}>
+      <div className="mt-10">
+        <h1 className="appHeader">To Do App</h1>
+        <Day />
+        <div className="d-flex align-items-center justify-content-center">
+          <input value={toDo} onChange={(e)=>setToDo(e.target.value)} className="m-2 rounded-pill searchbar no-outline p-1 bg-dark text-white" type="text" placeholder={"Add a task.."} />
+         
+          <div>
+            <button onClick={()=>{setToDos([...toDos,{id:Date.now(),text:toDo,status:false}]); setToDo('') }} className=" btn-add rounded-3">Add</button>
+          </div>
+        </div>
+        <Time date={time} />
+
+      </div>
+      <div className=" d-flex  ">
+        
+        <div className="col-md-6 m-2">
+          
+          <h1 className="text-warning TaskHeader">Pending Tasks</h1>
+          {
+            toDos.map((todo,index)=>{
+             if(!todo.status){
+              return (
+
+                <TaskList 
+                 key={index} 
+                todo={todo} 
+                setToDos={setToDos} 
+                toDos={toDos} />  
+              )
+            }
+            return null
+            })
+          }
+          </div> 
+          <div className="col-md-6 m-2">
+          <h1 className="text-success TaskHeader">Completed Task</h1>
+          {
+            toDos.map((obj,index)=>{
+              
+              if(obj.status){
+                return (
+                  <CompletedTaskList 
+                  key={index} 
+                 todo={obj} 
+                 setToDos={setToDos} 
+                 toDos={toDos} /> 
+                )
+              }
+              return null
+            })
+          }
+       
+          
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/taskList.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+
+// Functio to display Pending tasks
+function TaskList(props) {
+  return (
+    <div  className="  pendingTask rounded-pill d-flex justify-content-between align-items-center mt-3 ">
+    <p className=" mt-1 mb-1 mr-1 {if(props.todo.status)} text-dark fw-bold  pl-3" style={{paddingLeft: '1rem', overflow:'hidden' , textOverflow:'ellipsis'}}> {props.todo.text}</p>
+    <div className='d-flex'>
+    <div style={{backgroundColor:'green'}} className="rounded-circle m-1" >
+      <i className="fas fa-check m-2 text-white fs-5" onClick={()=>{
+      console.log(props.toDos)
+        props.setToDos(props.toDos.filter(obj=>{
+            if(obj.id === props.todo.id ){
+                obj.status = !props.todo.status;
+            }
+            return obj
+        }))
+    
+    }} value={props.todo.id}></i>
+    </div>
+    <div style={{backgroundColor:'red'}} className="rounded-circle m-1">
+      <i className="fas fa-trash-alt m-2 text-white fs-5"></i>
+    </div>
+    </div>
+</div>
+  )
+}
+
+// Function to display the time 
+function Time(props){
+    return (
+        <label htmlFor="" className="text-white fw-bold time-class ">{props.date}</label>
+    )
+}
+
+// Function to Display all the completed tasks
+function CompletedTaskList(props){
+    return(
+        <div  className=" completedTask rounded-pill d-flex justify-content-between align-items-center mt-3 ">
+        <p className=" mt-1 mb-1 mr-1  text-dark fw-bold  pl-3" style={{paddingLeft: '1rem', overflow:'hidden' , textDecoration:'line-through' ,textOverflow:'ellipsis'}}> {props.todo.text}.</p>
+        <div className='d-flex'>
+        {/* <div style={{backgroundColor:'orange'}} className="rounded-circle m-1" >
+          <i className="fas fa-check m-2 text-white fs-5" onClick={()=>{
+          
+            props.setToDos(props.toDos.filter(obj=>{
+                if(obj.id === props.todo.id ){
+                    obj.status = !props.todo.status;
+                }
+                return obj
+            }))
+        
+        }} value={props.todo.id}></i>
+        </div> */}
+        <div style={{backgroundColor:'red'}} className="rounded-circle m-1">
+          <i className="fas fa-trash-alt m-2 text-white fs-5" onClick={()=>{
+            props.setToDos(props.toDos.filter((obj)=>{
+                if(obj.id !== props.todo.id){
+                    return obj
+                }
+                return null;
+            }))
+            console.log(props.toDos)
+
+          }}></i>
+        </div>
+        </div>
+    </div>
+    )
+}
+
+//Functio to display only the Day
+function Day(){
+        const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        const emojis = ['🌞', '🌝', '🌟', '🌈', '☁️', '🌧️', '❄️']; 
+        const date = new Date();
+        const dayIndex = date.getDay();
+        const dayName = dayNames[dayIndex];
+        const emoji = emojis[dayIndex]; 
+    return (
+
+        <h4 className='text-warning day'>{dayName} {emoji}</h4>
+    )
+}
+
+export  {TaskList,Time,CompletedTaskList,Day}
